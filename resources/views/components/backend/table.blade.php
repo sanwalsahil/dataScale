650d1a976134d9b605c7eb377eb7b6ad868eb116
sahil sanwal <sanwalsahil2017@gmail.com>
2020-04-26 12:04:14 +0530

    frontedn of admin panel

---
+++ resources/views/components/backend/table.blade.php
@@ -0,0 +1,56 @@
+<table class="table">
+    <thead class=" text-primary">
+    <th>
+        ID
+    </th>
+    <th>
+        Name
+    </th>
+    <th>
+        Country
+    </th>
+    <th>
+        City
+    </th>
+    <th>
+        Salary
+    </th>
+    </thead>
+    <tbody>
+    <tr>
+        <td>
+            1
+        </td>
+        <td>
+            Dakota Rice
+        </td>
+        <td>
+            Niger
+        </td>
+        <td>
+            Oud-Turnhout
+        </td>
+        <td class="text-primary">
+            $36,738
+        </td>
+    </tr>
+    <tr>
+        <td>
+            2
+        </td>
+        <td>
+            Minerva Hooper
+        </td>
+        <td>
+            Curaçao
+        </td>
+        <td>
+            Sinaai-Waas
+        </td>
+        <td class="text-primary">
+            $23,789
+        </td>
+    </tr>
+
+    </tbody>
+</table>
--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,59 @@ Route::get('/companyDetails', function () {
 Route::get('/contactUs', function () {
     return view('frontend.contact_us');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+
+Route::group(['middleware' => 'auth'], function () {
+
+    /***** company lists ******/
+    Route::get('/all_companies',function(){
+        return view('pages.all_companies');
+    });
+
+    /***** user lists ******/
+    Route::get('/all_users',function(){
+        return view('pages.all_users');
+    });
+
+	Route::get('table-list', function () {
+		return view('pages.table_list');
+	})->name('table');
+
+	Route::get('typography', function () {
+		return view('pages.typography');
+	})->name('typography');
+
+	Route::get('icons', function () {
+		return view('pages.icons');
+	})->name('icons');
+
+	Route::get('map', function () {
+		return view('pages.map');
+	})->name('map');
+
+	Route::get('notifications', function () {
+		return view('pages.notifications');
+	})->name('notifications');
+
+	Route::get('rtl-support', function () {
+		return view('pages.language');
+	})->name('language');
+
+	Route::get('upgrade', function () {
+		return view('pages.upgrade');
+	})->name('upgrade');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+	Route::resource('user', 'UserController', ['except' => ['show']]);
+	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
+	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
+	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
+});
+
